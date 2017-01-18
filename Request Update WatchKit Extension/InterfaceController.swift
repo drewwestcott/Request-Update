23c4b5c8d2b649625c24405c702c6df79688fb06
Drew Westcott <code@drewwestcott.co.uk>
2017-01-18 19:05:33 +0000

    iPhone App can request update from Watch App and then receive reply. Downside both Apps have to be running

--- Request Update WatchKit Extension/InterfaceController.swift
+++ Request Update WatchKit Extension/InterfaceController.swift
@@ -7,19 +7,40 @@
 //
 
 import WatchKit
-import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
 
+	@IBOutlet var fuelSelectedLabel: WKInterfaceLabel!
+	var currentFuel = ""
+	let localStorage = UserDefaults.standard
+	
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
+		
+		if let fuel = localStorage.string(forKey: "fuel") {
+			currentFuel = fuel
+			if currentFuel == "cookie" {
+				fuelSelectedLabel.setText("🍪")
+			}
+			if currentFuel == "coffee" {
+				fuelSelectedLabel.setText("☕️")
+			}
+			if currentFuel == "pizza" {
+				fuelSelectedLabel.setText("🍕")
+			}
+		}
+	}
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+		if WCSession.isSupported() {
+			let session = WCSession.default()
+			session.delegate = self
+			session.activate()
+			print("watch activitaed")
+		}
         super.willActivate()
     }
     
@@ -28,4 +49,42 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+	@IBAction func coffeeTapped() {
+		fuelSelectedLabel.setText("☕️")
+		currentFuel = "coffee"
+		localStorage.set("coffee", forKey: "fuel")
+	}
+
+	@IBAction func cookieTapped() {
+		fuelSelectedLabel.setText("🍪")
+		currentFuel = "cookie"
+		localStorage.set("cookie", forKey: "fuel")
+	}
+
+	@IBAction func pizzaTapped() {
+		fuelSelectedLabel.setText("🍕")
+		currentFuel = "pizza"
+		localStorage.set("pizza", forKey: "fuel")
+	}
+	
+	func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+		
+		
+	}
+	
+	func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+		WKInterfaceDevice().play(.notification)
+		let messagetype = message["Message"] as? String
+		if messagetype == "needUpdate" {
+			sendReply()
+		}
+	}
+	
+	func sendReply() {
+		
+		let message = ["fuel":currentFuel]
+		print("Sent \(currentFuel)")
+		WCSession.default().sendMessage(message, replyHandler: nil, errorHandler: nil)
+		print("message sent")
+	}
 }
--- Request Update/ViewController.swift
+++ Request Update/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, WCSessionDelegate {
+	
+	@IBOutlet weak var textLabel: UILabel!
+	@IBOutlet weak var responseLabel: UILabel!
+	
+		override func viewDidLoad() {
+			super.viewDidLoad()
+			
+			if (WCSession.isSupported()) {
+				let watchSession = WCSession.default()
+				watchSession.delegate = self
+				watchSession.activate()
+				print("Activated")
+			}
+			
+		}
+		
+		override func didReceiveMemoryWarning() {
+			super.didReceiveMemoryWarning()
+			// Dispose of any resources that can be recreated.
+		}
+		
+		func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+			
+			if let watchMessage = applicationContext["watchMessage"] {
+				
+				print(watchMessage)
+				
+			}
+		}
+		
+		func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+			
+			print("Activated")
+			
+		}
+		
+		func sessionDidBecomeInactive(_ session: WCSession) {
+			
+			print("Session Inactive")
+			
+		}
+		
+		func sessionDidDeactivate(_ session: WCSession) {
+			
+			print("Session deactivated")
+			
+		}
+		
+		@IBAction func requestUpdate(_ sender: Any) {
+			
+			let message = ["Message":"needUpdate"]
+			WCSession.default().sendMessage(message, replyHandler: nil, errorHandler: nil)
+		}
+	
+	func waitingForUpdate() {
+		textLabel.text = "Waiting for reply…"
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+		
+		if let fuel = message["fuel"] as? String {
+			if fuel == "cookie" {
+				responseLabel.text = "🍪"
+			}
+			if fuel == "coffee" {
+				responseLabel.text = "☕️"
+			}
+			if fuel == "pizza" {
+				responseLabel.text = "🍕"
+			}
+		}
+		textLabel.text = ""
 	}
-
-
 }
-
